e10469229cac68f154f1fbed0adc136269ad3830
Vinicius Bortolo <luizviniciusbortolo@gmail.com>
2022-07-24 15:54:43 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,25 @@
+import { 
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import { AuthContextProvider } from "./contexts/AuthContext";
+
+import { Landing } from "./pages/Landing";
+
 export function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Landing />}>
+            <Route path="dashboard" element={<div>dashboard</div>}/>
+            <Route path="stats" element={<div>stats</div>}/>
+            <Route path="generator" element={<div>generator</div>}/>
+          </Route>
+        </Routes>
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
 }
