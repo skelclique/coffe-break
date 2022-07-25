f9f610db42a73f8eec3baf64dbcef79cd93ed584
Vinicius Bortolo <luizviniciusbortolo@gmail.com>
2022-07-24 23:56:42 -0300

    refactor: simplify Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,75 +1,20 @@
-import { useAuth } from '../../hooks/useAuth';
+import { Outlet } from 'react-router-dom';
 
-import { NavLink, Outlet } from 'react-router-dom';
+import { NavLinkIcon } from '../../components/NavLinkIcon';
 
-import { MdBarChart, MdDashboard, MdLogout, MdSettings } from 'react-icons/md';
+import './styles.scss';
 
 export function Home() {
-  const { setUser } = useAuth();
-
   return (
-    <div className="h-screen">
-      <div className="flex h-screen w-screen p-10">
-        <nav
-          className="flex flex-col items-center w-20
-          gap-6 py-6 pt-20 rounded-3xl bg-blue relative"
-        >
-          <NavLink to="dashboard" className="relative">
-            {({ isActive }) => (
-              <div
-                className={`${
-                  isActive
-                    ? 'bg-white flex items-center ml-4 pl-4 pr-6 rounded-l-2xl active-link'
-                    : 'bg-blue'
-                } py-3`}
-              >
-                <MdDashboard
-                  className={`${isActive ? 'text-blue' : 'text-white'} w-6 h-6`}
-                />
-              </div>
-            )}
-          </NavLink>
-          <NavLink to="stats" className="relative">
-            {({ isActive }) => (
-              <div
-                className={`${
-                  isActive
-                    ? 'bg-white flex items-center ml-4 pl-3 pr-5 rounded-l-2xl active-link'
-                    : 'bg-blue'
-                } py-2`}
-              >
-                <MdBarChart
-                  className={`${isActive ? 'text-blue' : 'text-white'} w-8 h-8`}
-                />
-              </div>
-            )}
-          </NavLink>
-          <NavLink to="generator" className="relative">
-            {({ isActive }) => (
-              <div
-                className={`${
-                  isActive
-                    ? 'bg-white flex items-center ml-4 pl-4 pr-6 rounded-l-2xl active-link'
-                    : 'bg-blue'
-                } py-3`}
-              >
-                <MdSettings
-                  className={`${isActive ? 'text-blue' : 'text-white'} w-6 h-6`}
-                />
-              </div>
-            )}
-          </NavLink>
-          <NavLink
-            to="/"
-            className="absolute bottom-8 cursor-pointer"
-            onClick={() => { setUser(false) }}
-          >
-            <MdLogout className="text-white w-6 h-6 flip-horizontal" />
-          </NavLink>
+    <div className="homeContainer">
+      <div className="homeApp-container">
+        <nav className="sideBar-container">
+          <NavLinkIcon path="dashboard" />
+          <NavLinkIcon path="stats" />
+          <NavLinkIcon path="generator" />
+          <NavLinkIcon path="/" />
         </nav>
-        <div className="pl-10">
-          <Outlet />
-        </div>
+        <Outlet />
       </div>
     </div>
   );
