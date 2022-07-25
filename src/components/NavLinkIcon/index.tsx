15c8ad6b8a8862122bc46ff96e753902bb6f3b35
Vinicius Bortolo <luizviniciusbortolo@gmail.com>
2022-07-25 00:39:06 -0300

    refactor: remove logOff

--- src/components/NavLinkIcon/index.tsx
+++ src/components/NavLinkIcon/index.tsx
@@ -1,25 +1,26 @@
 import { NavLink } from 'react-router-dom';
 
+import { AnchorHTMLAttributes } from 'react';
+
 import { useAuth } from '../../hooks/useAuth';
 
 import { MdBarChart, MdDashboard, MdLogout, MdSettings } from 'react-icons/md';
 
 import './styles.scss';
 
-type NavLinkIconType = {
+type NavLinkIconType = AnchorHTMLAttributes<HTMLAnchorElement> & {
   path: string;
 }
 
 export function NavLinkIcon({
   path,
+  ...rest
 }: NavLinkIconType) {
-  const { setUser } = useAuth();
-
   return (
     <NavLink 
-      to={path} 
-      className={`${ path == '/' ? 'absolute bottom-8 cursor-pointer' : 'sideNav-container'}`}
-      onClick={() => path == '/' && setUser(false)}
+      to={path}
+      className="sideNav-container"
+      {...rest}
     >
       {({ isActive }) => (
         <div className="py-3">
@@ -37,10 +38,6 @@ export function NavLinkIcon({
             <>
               <MdSettings className={`${isActive ? 'text-blue' : 'text-white'} icon-md`} />  
               <div className={`${isActive ? 'boxAnimation reverseBorder' : '' } boxEffect top-0`} />
-            </>
-          ) : path == '/' ? (
-            <>
-              <MdLogout className="text-white icon-md flip-x" />
             </>
           ) : null }
         </div>
